e9ecf35884a0defe0c296c9f9ceade211a219a07
colJY <ljy162518@gmail.com>
2023-10-16 17:13:27 +0900

    FEAT : TextInputLayout EndIcon 속성, Icon 추가

--- app/src/main/java/com/sas/companymanagement/ui/main/MainFragment.kt
+++ app/src/main/java/com/sas/companymanagement/ui/main/MainFragment.kt
@@ -1,29 +1,49 @@
 package com.sas.companymanagement.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sas.companymanagement.databinding.FragmentMainBinding
+import com.sas.companymanagement.ui.common.ViewBindingBaseFragment
+import com.sas.companymanagement.ui.schedule.update.ScheduleUpdateFragment
+import com.sas.companymanagement.ui.schedule.update.ScheduleUpdateViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
-class MainFragment : Fragment() {
-    private var _binding: FragmentMainBinding? = null
-    private val binding get() = _binding!!
+class MainFragment : ViewBindingBaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
+    companion object {
+        fun newInstance() = ScheduleUpdateFragment()
+    }
+
+    private lateinit var viewModel: ScheduleUpdateViewModel
+    private val compositeDisposable = CompositeDisposable()
+
+    val manager = LinearLayoutManager(
+        context,
+        LinearLayoutManager.VERTICAL,
+        false
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mainList = mutableListOf(
+            MainDataClass("2023.10.4","르세라핌","뮤직뱅크"),
+            MainDataClass("2023.10.15","르핌","뮤크")
+        )
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
+        with(binding.mainRv){
+            layoutManager = manager
+            adapter = MainRecyclerViewAdapter(mainList,requireActivity())
+        }
     }
 
     override fun onDestroyView() {
         _binding = null
+        compositeDisposable.clear()
         super.onDestroyView()
     }
+
+
 
 }
