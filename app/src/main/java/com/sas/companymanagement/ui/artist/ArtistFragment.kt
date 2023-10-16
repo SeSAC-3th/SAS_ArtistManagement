03eb5fc7ceb6481d62220364e3f0be09f6220ef5
yswn1531 <yswn1531@gmail.com>
2023-10-16 16:32:55 +0900

    [Feat] : Tab 변경

--- app/src/main/java/com/sas/companymanagement/ui/artist/ArtistFragment.kt
+++ app/src/main/java/com/sas/companymanagement/ui/artist/ArtistFragment.kt
@@ -8,7 +8,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.LinearLayout
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -23,10 +23,10 @@ class ArtistFragment : Fragment() {
     private var _binding: FragmentArtistBinding? = null
     private val binding get() = _binding!!
 
-    private var artistRecyclerView : RecyclerView? = null
-    private var artistGridLayoutManager : GridLayoutManager? = null
-    private var artistList : ArrayList<Artist>? = null
-    private var artistAdapter : ArtistAdapter? = null
+    private var artistRecyclerView: RecyclerView? = null
+    private var artistGridLayoutManager: GridLayoutManager? = null
+    private var artistList: ArrayList<Artist>? = null
+    private var artistAdapter: ArtistAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,39 +40,35 @@ class ArtistFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(binding){
+        with(binding) {
             artistRecyclerView = artistRV
-            artistGridLayoutManager = GridLayoutManager(context, 2, LinearLayoutManager.VERTICAL, false)
+            artistGridLayoutManager =
+                GridLayoutManager(context, 2, LinearLayoutManager.VERTICAL, false)
             artistRecyclerView?.layoutManager = artistGridLayoutManager
             artistRecyclerView?.setHasFixedSize(true)
             artistList = ArrayList()
             artistList = setDatainList()
-            artistAdapter = ArtistAdapter(requireContext(),artistList!!)
+            artistAdapter = ArtistAdapter(requireContext(), artistList!!)
             artistRecyclerView?.adapter = artistAdapter
+            setTabItemMargin(category, 30)
         }
 
-        val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(object : MenuProvider{
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.top_toolbar_menu,menu)
-            }
-
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                if (menuItem.itemId == R.id.btnAdd){
-                    //생성창으로 가는 동작
-                }
-                return true
-            }
-        }, viewLifecycleOwner)
-
         val tabLayout = binding.category
-        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener{
+        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 val param = when (tab.position) {
-                    0 -> { /*ALL*/}
-                    1 -> {/*가수*/}
-                    2 -> {/*배우*/}
-                    3 -> {/*탤런트*/}
+                    0 -> { /*ALL*/
+                    }
+
+                    1 -> {/*가수*/
+                    }
+
+                    2 -> {/*배우*/
+                    }
+
+                    3 -> {/*탤런트*/
+                    }
+
                     else -> throw IllegalStateException()
                 }
             }
@@ -82,24 +78,22 @@ class ArtistFragment : Fragment() {
         })
 
 
-
-
     }
 
-    private fun setDatainList() : ArrayList<Artist>{
+    private fun setDatainList(): ArrayList<Artist> {
 
         var items: ArrayList<Artist> = ArrayList()
 
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
-        items.add(Artist(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
+        items.add(Artist(R.drawable.ic_add_circle_24, "김채원"))
 
         return items
     }
@@ -108,6 +102,17 @@ class ArtistFragment : Fragment() {
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun setTabItemMargin(tabLayout: TabLayout, marginEnd: Int) {
+        val tabs = tabLayout.getChildAt(0) as ViewGroup
+        for (i in 0 until tabs.childCount) {
+            val tab = tabs.getChildAt(i)
+            val lp = tab.layoutParams as LinearLayout.LayoutParams
+            lp.marginEnd = marginEnd
+            tab.layoutParams = lp
+            tabLayout.requestLayout()
+        }
     }
 
 }
--- app/src/main/java/com/sas/companymanagement/ui/group/GroupFragment.kt
+++ app/src/main/java/com/sas/companymanagement/ui/group/GroupFragment.kt
@@ -1,6 +1,5 @@
 package com.sas.companymanagement.ui.group
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,13 +10,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sas.companymanagement.R
 import com.sas.companymanagement.databinding.FragmentGroupBinding
-import com.sas.companymanagement.databinding.FragmentMainBinding
+import com.sas.companymanagement.ui.artist.Artist
+import com.sas.companymanagement.ui.artist.ArtistAdapter
 
 class GroupFragment : Fragment() {
     private var _binding: FragmentGroupBinding? = null
     private val binding get() = _binding!!
+
+    private var groupRecyclerView : RecyclerView? = null
+    private var groupGridLayoutManager : GridLayoutManager? = null
+    private var groupList : ArrayList<Group>? = null
+    private var groupAdapter : GroupAdapter? = null
 
 
     override fun onCreateView(
@@ -32,27 +40,40 @@ class GroupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(object : MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.top_toolbar_menu,menu)
-            }
+        with(binding){
+            groupRecyclerView = groupRV
+            groupGridLayoutManager = GridLayoutManager(context, 2, LinearLayoutManager.VERTICAL, false)
+            groupRecyclerView?.layoutManager = groupGridLayoutManager
+            groupRecyclerView?.setHasFixedSize(true)
+            groupList = ArrayList()
+            groupList = setDatainList()
+            groupAdapter = GroupAdapter(requireContext(),groupList!!)
+            groupRecyclerView?.adapter = groupAdapter
+        }
 
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                if (menuItem.itemId == R.id.btnAdd){
-                    //생성창으로 가는 동작
-                }
-                return true
-            }
-        }, viewLifecycleOwner)
+    }
 
+    private fun setDatainList() : ArrayList<Group>{
+
+        var items: ArrayList<Group> = ArrayList()
+
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+        items.add(Group(R.drawable.ic_add_circle_24,"김채원"))
+
+        return items
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
-
-
 
 }
