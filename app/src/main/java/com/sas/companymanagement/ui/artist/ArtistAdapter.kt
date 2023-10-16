d6ec4fbfa3eea5d43d61b1143203f420f021773a
yswn1531 <yswn1531@gmail.com>
2023-10-16 16:38:28 +0900

    [Feat] : inner class 구현

--- app/src/main/java/com/sas/companymanagement/ui/artist/ArtistAdapter.kt
+++ app/src/main/java/com/sas/companymanagement/ui/artist/ArtistAdapter.kt
@@ -12,6 +12,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sas.companymanagement.R
 
 class ArtistAdapter(var context: Context, var arrayList: ArrayList<Artist>): RecyclerView.Adapter<ArtistAdapter.ItemHolder>() {
+    inner class ItemHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var images = itemView.findViewById<ImageView>(R.id.recyclerImage)
+        var artistNames = itemView.findViewById<TextView>(R.id.recyclerName)
+
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
         val itemHolder = LayoutInflater.from(parent.context)
             .inflate(R.layout.grid_layout_list_item, parent,false)
@@ -29,12 +35,6 @@ class ArtistAdapter(var context: Context, var arrayList: ArrayList<Artist>): Rec
         holder.images.setOnClickListener {
             Toast.makeText(context,artist.artistName,Toast.LENGTH_SHORT).show()
         }
-
-    }
-
-    class ItemHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        var images = itemView.findViewById<ImageView>(R.id.recyclerImage)
-        var artistNames = itemView.findViewById<TextView>(R.id.recyclerName)
 
     }
 
--- app/src/main/java/com/sas/companymanagement/ui/group/GroupAdapter.kt
+++ app/src/main/java/com/sas/companymanagement/ui/group/GroupAdapter.kt
@@ -11,6 +11,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sas.companymanagement.R
 
 class GroupAdapter(var context: Context, var arrayList: ArrayList<Group>): RecyclerView.Adapter<GroupAdapter.ItemHolder>() {
+
+    inner class ItemHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var images = itemView.findViewById<ImageView>(R.id.recyclerImage)
+        var artistNames = itemView.findViewById<TextView>(R.id.recyclerName)
+
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
         val itemHolder = LayoutInflater.from(parent.context)
             .inflate(R.layout.grid_layout_list_item, parent,false)
@@ -31,10 +39,6 @@ class GroupAdapter(var context: Context, var arrayList: ArrayList<Group>): Recyc
 
     }
 
-    class ItemHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        var images = itemView.findViewById<ImageView>(R.id.recyclerImage)
-        var artistNames = itemView.findViewById<TextView>(R.id.recyclerName)
 
-    }
 
 }
