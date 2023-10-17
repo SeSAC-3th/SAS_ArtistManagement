67726afaf3297a4b7da7647a53a22295e8bb36b0
lgy15 <lgy159@naver.com>
2023-10-17 14:07:59 +0900

    Feat : ArtistDetailFragment 수정, Artistadapter이용 / ScheduleAdapter 생성

---
+++ app/src/main/java/com/sas/companymanagement/ui/schedule/ScheduleAdapter.kt
@@ -0,0 +1,40 @@
+package com.sas.companymanagement.ui.schedule
+
+import android.content.Context
+import android.media.Image
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.sas.companymanagement.R
+
+class ScheduleAdapter(var context: Context, var arrayList: MutableList<Schedule>) :
+    RecyclerView.Adapter<ScheduleAdapter.ItemHolder>() {
+    inner class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var scheduleNames = itemView.findViewById<TextView>(R.id.schedule_name)
+        var scheduleDates = itemView.findViewById<TextView>(R.id.schedule_date)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val itemHolder = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_schedule_horizontal, parent, false)
+        return ItemHolder(itemHolder)
+    }
+
+    override fun getItemCount() = arrayList.size
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+
+        var schedule: Schedule = arrayList.get(position)
+        Log.e("position", "${position}")
+        Log.e("arrayList", "${arrayList.get(0)}")
+        Log.e("size", "${getItemCount()}")
+        Log.e("name", "${schedule.scheduleName}")
+        holder.scheduleNames.text = schedule.scheduleName
+        holder.scheduleDates.text = schedule.scheduleDate
+    }
+}
