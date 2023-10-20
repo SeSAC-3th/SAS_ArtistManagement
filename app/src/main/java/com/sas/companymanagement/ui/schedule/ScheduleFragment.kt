45dab7b819d19b5291db22d505f706d8bdcfa974
sia7997 <sia7997@naver.com>
2023-10-20 14:23:11 +0900

    Chore : 스케쥴, 스케쥴 상세 화면 전환

--- app/src/main/java/com/sas/companymanagement/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/sas/companymanagement/ui/schedule/ScheduleFragment.kt
@@ -1,24 +1,99 @@
 package com.sas.companymanagement.ui.schedule
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.jakewharton.rxbinding4.view.clicks
+import com.sas.companymanagement.R
+import com.sas.companymanagement.databinding.FragmentArtistBinding
 import com.sas.companymanagement.databinding.FragmentScheduleBinding
+import com.sas.companymanagement.ui.artist.Artist
+import com.sas.companymanagement.ui.artist.ArtistAdapter
+import com.sas.companymanagement.ui.artist.ArtistFragmentDirections
+import com.sas.companymanagement.ui.artist.ArtistViewModel
+import com.sas.companymanagement.ui.common.ViewBindingBaseFragment
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-class ScheduleFragment : Fragment() {
-    private var _binding: FragmentScheduleBinding? = null
-    private val binding get() = _binding!!
+class ScheduleFragment :  ViewBindingBaseFragment<FragmentScheduleBinding>(FragmentScheduleBinding::inflate) {
 
-
+    private val compositeDisposable = CompositeDisposable()
+    private var scheduleList: MutableList<TodaySchedule>? = null
+    private var scheduleAdapter = TodayScheduleAdapter(
+        navigateScheduleDetail = {scheduleId -> navigateScheduleDetail(scheduleId)}
+    )
+    private val viewModel: ScheduleViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentScheduleBinding.inflate(inflater, container, false)
+
+        settingScheduleRecyclerView()
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(compositeDisposable) {
+            with(binding) {
+                btnScheduleAdd
+                    .clicks()
+                    .observeOn(Schedulers.io())
+                    .throttleFirst(500, TimeUnit.MILLISECONDS)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                       val action = ScheduleFragmentDirections.actionFragmentScheduleToScheduleUpdateFragment(0)
+                        //register면 id가 없는데 이 경우는 어떻게 하지..
+                        findNavController().navigate(action)
+                    }, {
+                        Log.e("RX_ERROR", compositeDisposable.toString())
+                    })
+            }
+        }
+    }
+
+    private fun navigateScheduleDetail(id: Int){
+        val action = ScheduleFragmentDirections.actionFragmentScheduleToScheduleDetailFragment(id)
+        findNavController().navigate(action)
+    }
+
+
+    private fun settingScheduleRecyclerView() {
+        with(binding) {
+            scheduleList = setDataInList()
+            scheduleAdapter.setScheduleList(scheduleList!!)
+//            scheduleAdapter = TodayScheduleAdapter(requireContext(), scheduleList!!, requireParentFragment())
+            rvScheduleToday.adapter = scheduleAdapter
+        }
+    }
+
+    private fun setDataInList(): MutableList<TodaySchedule> {
+
+        val items: MutableList<TodaySchedule> = mutableListOf()
+
+        items.add(TodaySchedule("르세라핌 뮤직뱅크 09:00~12:00"))
+        items.add(TodaySchedule("르세라핌 뮤직뱅크 09:00~12:00"))
+        items.add(TodaySchedule("르세라핌 뮤직뱅크 09:00~12:00"))
+
+//        viewModel.getAllArtists()?.observe(viewLifecycleOwner) { Artists ->
+//            Artists.forEach { artist ->
+//                items.add(Artist(artist.artistName!!, "src"))
+//            }
+//        }
+        return items
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/sas/companymanagement/ui/schedule/detail/ScheduleDetailFragment.kt
+++ app/src/main/java/com/sas/companymanagement/ui/schedule/detail/ScheduleDetailFragment.kt
@@ -2,17 +2,32 @@ package com.sas.companymanagement.ui.schedule.detail
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jakewharton.rxbinding4.view.clicks
 import com.sas.companymanagement.R
+import com.sas.companymanagement.databinding.FragmentGroupDetailBinding
+import com.sas.companymanagement.databinding.FragmentScheduleDetailBinding
 import com.sas.companymanagement.databinding.FragmentScheduleUpdateBinding
+import com.sas.companymanagement.ui.artist.Artist
+import com.sas.companymanagement.ui.artist.ArtistAdapter
+import com.sas.companymanagement.ui.artist.ArtistFragmentDirections
 import com.sas.companymanagement.ui.common.ViewBindingBaseFragment
+import com.sas.companymanagement.ui.schedule.ScheduleAdapter
+import com.sas.companymanagement.ui.schedule.ScheduleFragmentDirections
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class ScheduleDetailFragment :
-    ViewBindingBaseFragment<FragmentScheduleUpdateBinding>(FragmentScheduleUpdateBinding::inflate) {
+    ViewBindingBaseFragment<FragmentScheduleDetailBinding>(FragmentScheduleDetailBinding::inflate) {
 
     companion object {
         fun newInstance() = ScheduleDetailFragment()
@@ -25,11 +40,25 @@ class ScheduleDetailFragment :
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_schedule_detail, container, false)
+        _binding = FragmentScheduleDetailBinding.inflate(inflater, container, false)
+
+        listenerSetup()
+        return binding.root
+    }
+
+    private fun listenerSetup() {
+        binding.tbScheduleDetail.setOnMenuItemClickListener { item ->
+            if (item.itemId == R.id.update) {
+                val action = ScheduleDetailFragmentDirections.actionScheduleDetailFragmentToScheduleUpdateFragment(0)
+                findNavController().navigate(action)
+            }
+            true
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 
     override fun onDestroyView() {
